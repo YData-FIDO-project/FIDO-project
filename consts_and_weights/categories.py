440657114abe1a6cc772b9ba77adf97eaf1ac5ef
Valeriya Vazhnova <61866710+valeriya-vazh@users.noreply.github.com>
2024-06-29 21:58:52 +0300

    Create categories.py

---
+++ consts_and_weights/categories.py
@@ -0,0 +1,20 @@
+"""
+All categories currently used in the app
+"""
+
+ALL_CATEGORIES_LIST = [
+  Appointment Letter, 
+  Bank Statement, 
+  Birth Certificate,
+  Electricity Bill,
+  Form 3,
+  Form 4,
+  Form A,
+  Introductory Letter,
+  Mortgage Statement,
+  Payslip,
+  Property Rate,
+  SSNIT Pension Statement,
+  Tenancy Agreement,
+  Water Bill,
+]
