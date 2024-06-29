69183e08ed0281887f8089d308d3e92e29293530
Valeriya Vazhnova <61866710+valeriya-vazh@users.noreply.github.com>
2024-06-29 23:12:30 +0300

    Create pdf_utils.py

---
+++ utils/pdf_utils.py
@@ -0,0 +1,26 @@
+"""
+Necessary functions for working with PDFs
+"""
+
+def converting_pdf_to_jpg(file):
+  """
+  Converting PDF file to image
+  :param file: PDF file
+
+  :returns: JPG image
+  """
+  img = ...
+  return img
+
+def extracting_text_from_pdf(file):
+  """
+  Extracting text from PDF file
+  :param file: PDF file
+
+  returns: text (str)
+  """
+  text = ...
+  if text:  # if not empty string
+    return text
+  else:
+    return None
