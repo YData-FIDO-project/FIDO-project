e339a5686bd2f904e2a2d4d4bc779288a76ce5b8
Valeriya Vazhnova <61866710+valeriya-vazh@users.noreply.github.com>
2024-06-29 21:48:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Placeholder for the main file with the project pipeline
+"""
+
+import fire
+import os
+import numpy as np
+import pandas as pd
+
+...
+
+if __name__ == '__main__':
+    fire.Fire(main)
