1e13cb5a636d91686c33389075aaae27f154df16
Valeriya Vazhnova <lera@Valeriyas-MacBook.local>
2024-07-12 20:31:39 +0300

    Modified project utils file

--- utils/project_utils.py
+++ utils/project_utils.py
@@ -1,11 +1,13 @@
 """
 Storing all helper functions
 """
+from rapidfuzz import fuzz, process
+import re
 
 from consts_and_weights.categories import ALL_CATEGORIES_LIST
 
 
-def matching_category(input_category: str, predicted_category: str):
+def matching_category(input_category: str, predicted_category: str) -> bool:
     """
     Checking if document was uploaded with the right category.
 
@@ -15,20 +17,51 @@ def matching_category(input_category: str, predicted_category: str):
     :returns: True / False (bool)
     """
 
-    input_category, predicted_category = input_category.strip().lower(), predicted_category.strip().lower()
+    # preprocessing category names
+    input_category, predicted_category = input_category.strip().lower().replace(' ', '_'),\
+                                         predicted_category.strip().lower().replace(' ', '_')
 
+    # asserting that all categories are known to the model
     assert input_category in ALL_CATEGORIES_LIST, "Unfamiliar input category"
     assert predicted_category in ALL_CATEGORIES_LIST, "Unfamiliar predicted category"
 
     return input_category == predicted_category
 
 
-def matching_name(input_name: str, text: str):
+def matching_name(input_name: str, text: str, threshold: int = 80):
     """
     Checking if uploaded document contains name of the customer
+
     :param input_name: customer name in the database
     :param text: text extracted from the uploaded document
-    :returns: True / False (bool)
+    :param threshold: threshold value for fuzzywuzz similarity score
+
+    :returns: matches (dictionary with name parts and similarity score), total matching score
     """
 
-    pass
+    # for empty input
+    if not input_name or not text:
+        print('No name or document text provided')
+        return {}, 0.0
+
+    # preprocessing the name
+    name_preprocessed = re.sub(r'\s*-\s*', ' ', input_name.strip().upper())
+    name_preprocessed = name_preprocessed.split()
+
+    # preprocessing the text
+    text = re.sub(r'[^\w\s\d]+', '', text.upper())
+
+    matches = {}
+    total_score = 0
+
+    for name_part in name_preprocessed:
+        match_score = fuzz.partial_ratio(name_part, text)
+        matches[name_part] = round(match_score, 2)
+        if match_score >= threshold:
+            total_score += match_score
+
+    total_score /= (100 * len(name_preprocessed))
+    print(f'Confidence level: {total_score :.0%}')
+
+    return matches, total_score
+
