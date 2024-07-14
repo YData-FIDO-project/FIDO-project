f0a9191118e8077a099ead07af4deaa8fc0c54dd
Valeriya Vazhnova <lera@Valeriyas-MacBook.local>
2024-07-14 13:00:03 +0300

    Added BERT inference function

---
+++ consts_and_weights/labels.py
@@ -0,0 +1,20 @@
+"""
+Dictionary mapping digits to category names
+"""
+
+CATEGORY_NAME_DICT = {
+    0: 'appointment_letter',
+    1: 'bank_statement',
+    2: 'birth_certificate',
+    3: 'electricity_bill',
+    4: 'form_3',
+    5: 'form_a',
+    6: 'introductory_letter',
+    7: 'mortgage_statement',
+    8: 'payslip',
+    9: 'property_rate',
+    10: 'rejected',
+    11: 'ssnit_pension_statement',
+    12: 'tenancy_agreement',
+    13: 'water_bill'
+}
--- models/bert_inference.py
+++ models/bert_inference.py
@@ -1,24 +1,63 @@
 """
 Inference on BERT model
 """
-
-# TODO: check which packages are actually needed
-import os
 import torch
-import random
-import copy
-import time
+from transformers import BertTokenizer
 
-from torch import nn
-from torch.utils.data import DataLoader, Dataset
-from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+from models.BERT_classifier import BERTClassifier
+from consts_and_weights.labels import CATEGORY_NAME_DICT
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.preprocessing import LabelEncoder
 
-import numpy as np
-import pandas as pd
+BERT_MODEL_NAME = 'bert-base-uncased'
+n_classes = len(CATEGORY_NAME_DICT)
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+# to GPU
+device = torch.device('mps' if torch.backends.mps.is_available() else
+                      torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+print(f"Device: {device}")
+
+# initializing the model
+model = BERTClassifier(bert_model_name=BERT_MODEL_NAME,
+                       num_classes=n_classes
+                       ).to(device)
+
+# using pretrained weights
+model.load_state_dict(torch.load(
+  'consts_and_weights/bert_13classes_10epochs_adam_full_data_with_rejected.pth'))
+
+# initializing the tokenizer
+tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_NAME)
+
+
+def inference(clf_model, model_tokenizer, document: str, label_dict: dict = CATEGORY_NAME_DICT):
+    """
+    Classifying provided document
+
+    :param clf_model: pretrained classifier
+    :param model_tokenizer: suitable tokenizer for the model
+    :param document: document to classify
+    :param label_dict: dictionary with category names (digit to label)
+
+    :returns: category (digit), softmax score
+    """
+
+    clf_model.eval()
+
+    # tokenizing input document
+    inputs = model_tokenizer(document,
+                             return_tensors='pt', truncation=True,
+                             padding=True, max_length=512)
+    input_ids = inputs['input_ids'].to(device)
+    attention_mask = inputs['attention_mask'].to(device)
+
+    with torch.no_grad():
+        outputs = clf_model(input_ids=input_ids, attention_mask=attention_mask)
+        softmax_outputs = torch.nn.functional.softmax(outputs, dim=1)
+        probabilities = softmax_outputs.cpu().detach().numpy()[0]
+
+        pred_category = probabilities.argmax()
+        probability = probabilities[pred_category]
+
+    print(f'Prediction: {label_dict[pred_category]} ({probability :,.3f}) ')
+    return pred_category, probability
+
