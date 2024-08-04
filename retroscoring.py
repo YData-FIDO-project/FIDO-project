4b0191a0d7176e9e5d594af5d123266cb6101761
Valeriya Vazhnova <lera@Valeriyas-MacBook.local>
2024-08-04 11:49:52 +0300

    Name matching for retroscoring

--- retroscoring.py
+++ retroscoring.py
@@ -8,23 +8,18 @@ Date: 2024-08-04
 import fire
 import pandas as pd
 
-from consts_and_weights.categories import ALL_CATEGORIES_LIST
-from utils.AWS_utils import downloading_image_from_s3
-from utils.text_utils import image_to_text_pipeline
-from utils.pdf_utils import *
 from utils.random_state_utils import fix_randomization
 from models.NLP.bert_testing import bert_testing
 from models.CV.mobilenet_testing import mobilenet_testing
 from models.ensemble import ensemble_testing
-from utils.project_utils import matching_category, matching_name
+from utils.project_utils import matching_name
 from utils.evaluation_utils import *
-from consts_and_weights.labels import CATEGORY_NAME_DICT
+
 
 LOCAL_DIR = 'outputs'
 
 
-def main(df: pd.DataFrame, key_id: str, secret_access_key: str,
-         test_mode: bool = False, label_dict: dict = CATEGORY_NAME_DICT,
+def main(df: pd.DataFrame, test_mode: bool = False,
          random_state: int = None, local_dir: str = LOCAL_DIR):
     """
     Validating a batch of input documents
@@ -32,11 +27,8 @@ def main(df: pd.DataFrame, key_id: str, secret_access_key: str,
     :param df: df with image metadata; assumes columns: 'uri', 'local_path', 'file_name',
     'text' (with extracted text), 'customer_name' (customer name). If test_mode, also 'label'
     (with label as a digit)
-    :param key_id: key id to access AWS
-    :param secret_access_key: secret access key to access AWS
     :param test_mode: if we are testing the model performance
     (i.e. ground truth label available) or not (i.e. no labels)
-    :param label_dict: dictionary with category names (digit to label)
     :param random_state: random state seed if needed (default None)
     :param local_dir: path to local directory in which we save the file
 
@@ -78,30 +70,21 @@ def main(df: pd.DataFrame, key_id: str, secret_access_key: str,
     print('\n--- CALCULATING ENSEMBLE SCORE ---')
     df_ensemble = ensemble_testing(df_nlp=df_nlp, df_cv=df_cv, test_mode=test_mode)
 
-    # TODO: add stuff here
     # category matching
-    category_match = matching_category(
-        input_category=input_category,
-        predicted_category=df_ensemble["prediction_cat_name"][0]
-    )
-    img_info['category_is_correct'] = category_match
-    if category_match:
-        print('\n- Category: correct')
-    else:
-        print('\n- ATTENTION: category mismatch')
+    df_ensemble['category_is_correct'] = df_ensemble['ground_truth'] == df_ensemble['final_prediction']
 
     # name matching
-    name_match, name_parts, name_score = matching_name(input_name=customer_name,
-                                                       text=img_info['text'])
-    img_info['name_is_correct'] = name_match
-    img_info['name_confidence'] = name_score
-    if name_match:
-        print(f'\n- Name: correct')
-    else:
-        print('\n- ATTENTION: name mismatch')
+    # a wrapper function to unpack the results
+    def apply_matching_name(row):
+        found_name, _, total_score = matching_name(row['name'], row['text'])
+        return pd.Series({'name_is_correct': found_name, 'name_confidence': total_score})
+
+    df_ensemble[[
+        'name_is_correct', 'name_confidence'
+    ]] = df_ensemble.apply(apply_matching_name, axis=1)
 
     # compile file with results
-    df_final = pd.DataFrame([img_info]).merge(df_ensemble, on='file_name')
+    df_final = df.merge(df_ensemble, on='file_name', how='inner')
 
     # save file with results locally
     final_file_path = os.path.join(local_dir, 'df_results.csv')
